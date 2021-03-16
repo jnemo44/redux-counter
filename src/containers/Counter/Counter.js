f8128ed6008f015c2b950957cf8733764028c54f
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2021-03-15 20:41:26 -0500

    Split reducer into two parts, and then combined them

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -4,6 +4,7 @@ import { connect }from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionTypes from '../../store/action';
 
 class Counter extends Component {
     state = {
@@ -36,7 +37,7 @@ class Counter extends Component {
                 <CounterControl label="Add 10" clicked={this.props.onAdd}  />
                 <CounterControl label="Subtract 10" clicked={this.props.onSubtract}  />
                 <hr />
-                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(strResult => (
                         <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
@@ -49,19 +50,19 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter,
-        storedResults: state.results
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
-        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-        onAdd: () => dispatch({type: 'ADD', value: 10}),
-        onSubtract: () => dispatch({type: 'SUBTRACT', value: 10}),
-        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
-        onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', resultElId: id})
+        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
+        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
+        onAdd: () => dispatch({type: actionTypes.ADD, value: 10}),
+        onSubtract: () => dispatch({type: actionTypes.SUBTRACT, value: 10}),
+        onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
+        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElId: id})
     };
 };
 
--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,8 +1,7 @@
-import * as actionTypes from './action';
+import * as actionTypes from '../action';
 
 const initialState = {
-    counter: 0,
-    results: []
+    counter: 0
 }
 
 const reducer = (state = initialState, action) => {
@@ -27,19 +26,6 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 counter: state.counter - action.value
-            }
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: state.counter})
-            }
-        case actionTypes.DELETE_RESULT:
-            // This how to delete elements from an array immutibly
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                    ...state,
-                    results: updatedArray
-                    
             }
     } 
     return state;
