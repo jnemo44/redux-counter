55d428021b99e403d45096805d3547e650e1bb8a
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2021-03-25 20:25:01 -0500

    Added redux-thunk to allow for async action creators

--- src/store/actions/action.js
+++ src/store/actions/action.js
@@ -31,11 +31,21 @@ export const subtract = (value) => {
     };
 };
 
-export const storeResult = (res) => {
+export const saveResult = ( res ) => {
     return {
         type: STORE_RESULT,
         result: res
-    };
+    }; 
+}
+
+// Async Action creator enabled by redux-thunk works by calling a
+// syncronous action creator
+export const storeResult = ( res ) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(saveResult(res));
+        },2000);
+    }
 };
 
 export const deleteResult = (id) => {
